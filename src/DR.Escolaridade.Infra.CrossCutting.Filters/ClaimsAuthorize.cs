feb3ee3dd6ec06a40e63ae36eca593a1717b8aba
denisiodrigues <denisio@ymail.com>
2020-04-13 11:30:30 -0300

    Autorização com foco em Claims

---
+++ src/DR.Escolaridade.Infra.CrossCutting.Filters/ClaimsAuthorize.cs
@@ -0,0 +1,38 @@
+﻿using System.Net;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DR.Escolaridade.Infra.CrossCutting.Filters
+{
+    public class ClaimsAuthorize : AuthorizeAttribute
+    {
+        public static string _claimName;
+        public static string _claimValue;
+
+        public ClaimsAuthorize(string claimName, string claimValue)
+        {
+            _claimName = claimName;
+            _claimValue = claimValue;
+        }
+
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            ClaimsIdentity userClaims = (ClaimsIdentity)httpContext.User.Identity; 
+            return ClaimsHelper.ValidarClaimsUsuario(userClaims, _claimName, _claimValue);
+        }
+
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+            //Se já está autenticado vai para a página de não aturizado
+            if (filterContext.HttpContext.Request.IsAuthenticated)
+            {
+                filterContext.Result = new HttpStatusCodeResult(HttpStatusCode.Forbidden);
+            }
+            else
+            {
+                base.HandleUnauthorizedRequest(filterContext);
+            }
+        }
+    }
+}
