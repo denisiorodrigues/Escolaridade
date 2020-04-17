3626ee70ca7f613e80f1eb9195b8a0bf17ed77ed
denisiodrigues <denisio@ymail.com>
2020-04-17 16:36:05 -0300

    Correção do bug de autorização

--- src/DR.Escolaridade.Infra.CrossCutting.Filters/ClaimsAuthorize.cs
+++ src/DR.Escolaridade.Infra.CrossCutting.Filters/ClaimsAuthorize.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System.Linq;
+using System.Net;
 using System.Security.Claims;
 using System.Web;
 using System.Web.Mvc;
@@ -18,8 +19,11 @@ namespace DR.Escolaridade.Infra.CrossCutting.Filters
 
         protected override bool AuthorizeCore(HttpContextBase httpContext)
         {
-            ClaimsIdentity userClaims = (ClaimsIdentity)httpContext.User.Identity; 
-            return ClaimsHelper.ValidarClaimsUsuario(userClaims, _claimName, _claimValue);
+            //ClaimsIdentity userClaims = (ClaimsIdentity)httpContext.User.Identity; 
+            //return ClaimsHelper.ValidarClaimsUsuario(userClaims, _claimName, _claimValue);
+            var identity = (ClaimsIdentity)httpContext.User.Identity;
+            var claim = identity.Claims.FirstOrDefault(c => c.Type == _claimName);
+            return claim != null && claim.Value.Contains(_claimValue);
         }
 
         protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
--- src/DR.Escolaridade.Infra.CrossCutting.Filters/ClaimsHelper.cs
+++ src/DR.Escolaridade.Infra.CrossCutting.Filters/ClaimsHelper.cs
@@ -14,29 +14,23 @@ namespace DR.Escolaridade.Infra.CrossCutting.Filters
         //Quando colocar o nome "this" dentrod o parâmertro indormo que esse métoo vais er ciado dentroda classe "MvcHtmlString" por exemplo
         public static MvcHtmlString IfClaimHelper(this MvcHtmlString value, string claimName, string claimValue)
         {
-            ClaimsIdentity userClaims = (ClaimsIdentity)HttpContext.Current.User.Identity;
-
-            return ValidarClaimsUsuario(userClaims,claimName, claimValue) ? value : MvcHtmlString.Empty;
+            return ValidarClaimsUsuario(claimName, claimValue) ? value : MvcHtmlString.Empty;
         }
 
         public static bool IfClaim(this WebViewPage value, string claimName, string claimValue)
         {
-            ClaimsIdentity userClaims = (ClaimsIdentity)HttpContext.Current.User.Identity;
-
-            return ValidarClaimsUsuario(userClaims, claimName, claimValue);
+            return ValidarClaimsUsuario(claimName, claimValue);
         }
 
         public static string IfClaimShow(this WebViewPage value, string claimName, string claimValue)
         {
-            ClaimsIdentity userClaims = (ClaimsIdentity)HttpContext.Current.User.Identity;
-
-            return ValidarClaimsUsuario(userClaims, claimName, claimValue) ? string.Empty : "disabled";
+            return ValidarClaimsUsuario(claimName, claimValue) ? string.Empty : "disabled";
         }
 
-        public static bool ValidarClaimsUsuario(ClaimsIdentity claimsIdentity, string claimName, string claimValue)
+        public static bool ValidarClaimsUsuario(string claimName, string claimValue)
         {
-            Claim claim = claimsIdentity.Claims.FirstOrDefault(c => c.Type == claimName);
-
+            var identity = (ClaimsIdentity)HttpContext.Current.User.Identity;
+            var claim = identity.Claims.FirstOrDefault(c => c.Type == claimName);
             return claim != null && claim.Value.Contains(claimValue);
         }
     }
